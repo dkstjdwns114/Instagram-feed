f16b76073ae5ca31324989282ac67a424d28bf05
dkstjdwns114 <sungjoon256@gmail.com>
2020-11-04 03:52:47 +0900

    Get json data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import ProfileImg from "./ProfileImg";
+import Thumbnails from "./Thumbnails";
+
 function App() {
   return (
     <div className="App">
       <h2>Hello World!!</h2>
+      <ProfileImg></ProfileImg>
+      <Thumbnails></Thumbnails>
     </div>
   );
 }
