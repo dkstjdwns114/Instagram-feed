cd92072edfa28ccc4dc7216f493fd654b4e7c19d
dkstjdwns114 <sungjoon256@gmail.com>
2020-11-12 00:23:56 +0900

    엔터누르면 댓글 써지게, submit하면 input에 댓글 없어지게

--- src/replies.js
+++ src/replies.js
@@ -5,16 +5,19 @@ class Replies extends Component {
     super(props);
     this.state = {
       item: "",
-      items: Object.values(this.props.replies)
+      items: Object.values(this.props.replies),
+      inputdefault: ""
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.submitItem = this.submitItem.bind(this);
+    this.onEnterPress = this.onEnterPress.bind(this);
   }
   handleInputChange(e) {
     let name = e.target.name;
     let value = e.target.value;
+    let userid = "admin";
 
-    this.setState({ [name]: value });
+    this.setState({ [name]: { id: userid, reply: value } });
   }
   submitItem(e) {
     e.preventDefault();
@@ -22,15 +25,42 @@ class Replies extends Component {
     let item = this.state.item;
     items.push(item);
     this.setState({ items: items });
+    this.setState({ item: { id: "", reply: "" } });
+  }
+  onEnterPress(e) {
+    if (e.keyCode === 13 && e.shiftKey === false) {
+      e.preventDefault();
+
+      //---------------------------
+      // 이부분에서 위에있는 submitItem을 호출하고싶다.
+      let items = this.state.items;
+      let item = this.state.item;
+      items.push(item);
+      this.setState({ items: items });
+      this.setState({ item: { id: "", reply: "" } });
+      //----------------------------
+    }
   }
 
   render() {
     return (
       <>
-        <input type="text" name="item" onChange={this.handleInputChange} />
+        <p>댓글 갯수 : {this.state.items.length}개</p>
+        <input
+          value={this.state.item.reply}
+          type="text"
+          name="item"
+          onChange={this.handleInputChange}
+          onKeyDown={this.onEnterPress}
+        />
         <input type="submit" value="Submit" onClick={this.submitItem} />
         {this.state.items.map((item, index) => {
-          return <p key={index}>{item}</p>;
+          return (
+            <p key={index}>
+              <span>{item.id} : </span>
+              {item.reply}
+            </p>
+          );
         })}
       </>
     );
