c622e18320d4f5479f1ce25111031fbab52895fd
dkstjdwns114 <sungjoon256@gmail.com>
2020-11-18 01:57:39 +0900

    프로필 상세페이지 데이터 출력(아직 페이지 이동은 안됨)

--- src/ProfileDetail.js
+++ src/ProfileDetail.js
@@ -2,22 +2,29 @@ import React from "react";
 import PostData from "./data.json";
 // import { Route, Link } from "react-router-dom";
 import Profile from "./Profile";
+import ProfileDetailThumbnails from "./ProfileDetailThumbnails";
 
 export default function ProfileDetail() {
   let userProfile;
+  let detailThumbnails = [];
   PostData.post_list.map((post_list, index) => {
     if (post_list.id === "apple") {
       userProfile = (
         <Profile profile_image={post_list.profile_image} id={post_list.id} />
       );
+      detailThumbnails.unshift({ post_list });
     }
     return userProfile;
   });
-  console.log(Object.values(userProfile));
+  console.log("detailThumbnails :", detailThumbnails);
   return (
     <div>
       <p>사용자의 이름이나 프로필 사진을 클릭했을때 연결할 페이지 입니다.</p>
       {userProfile}
+      <ProfileDetailThumbnails
+        userId={"apple"}
+        userInfo={detailThumbnails}
+      ></ProfileDetailThumbnails>
     </div>
   );
 }
---
+++ src/ProfileDetailThumbnails.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function ProfileDetailThumbnails({ userId, userInfo }) {
+  let info = userInfo.map((information, inx) => {
+    return Object.values(information);
+  });
+  let thumbnailArr = [];
+  for (let i = 0; i < info.length; i++) {
+    thumbnailArr.push(info[i][0]["photo_thumbnails"][0]);
+  }
+
+  let thumbnailList = [];
+
+  thumbnailArr.map((thumbnail, index) => {
+    thumbnail.type === "image"
+      ? (index + 1) % 3 === 0
+        ? thumbnailList.push(
+            <>
+              <img src={thumbnail.thumbnails} alt={`${index + 1}번 이미지`} />
+              <br />
+            </>
+          )
+        : thumbnailList.push(
+            <img src={thumbnail.thumbnails} alt={`${index + 1}번 이미지`} />
+          )
+      : (index + 1) % 3 === 0
+      ? thumbnailList.push(
+          <>
+            <video controls loop muted width="140" height="140">
+              <source src={thumbnail.thumbnails} type="video/mp4" />
+            </video>
+            <br />
+          </>
+        )
+      : thumbnailList.push(
+          <>
+            <video controls loop muted width="140" height="140">
+              <source src={thumbnail.thumbnails} type="video/mp4" />
+            </video>
+          </>
+        );
+  });
+  return (
+    <div>
+      <p>게시물 : {thumbnailArr.length}개</p>
+      {thumbnailList}
+    </div>
+  );
+}
