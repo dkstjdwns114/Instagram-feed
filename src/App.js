843ddbaeca7270066bab6f6e2e265946c434f433
dkstjdwns114 <sungjoon256@gmail.com>
2020-11-20 02:44:42 +0900

    게시물 상세페이지 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Post from "./Post";
 import ProfileDetail from "./ProfileDetail";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PostDetail from "./PostDetail";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Post} />
             <Route exact path="/profileDetail/:id" component={ProfileDetail} />
+            <Route exact path="/postDetail/:idx" component={PostDetail} />
           </Switch>
         </div>
       </Router>
